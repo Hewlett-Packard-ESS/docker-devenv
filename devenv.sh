9b27b05b546a5386387d7320af6813840588f75c
Karl Stoney <karl@jambr.co.uk>
2014-12-11 11:37:14 -0500

    Fixes to the welcome message

--- devenv.sh
+++ devenv.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
-function hcenter {
- 
-  text="$1"
- 
-  cols=`tput cols`
- 
-  IFS=$'\n'$'\r'
-  for line in $(echo -e $text); do
- 
-    line_length=`echo $line| wc -c`
-    half_of_line_length=`expr $line_length / 2`
-    center=`expr \( $cols / 2 \) - $half_of_line_length`
- 
-    spaces=""
-    for ((i=0; i < $center; i++)) {
-      spaces="$spaces "
-    }
- 
-    echo "$spaces$line"
- 
-  done
- 
-}
-
 export TERM='xterm-256color'
-hcenter "Welcome to the HP ESS Development Environment!"
-hcenter "**********************************************"
-. /root/.bashrc
+echo "****************************************************"
+echo "*  Welcome to the HP ESS Development Environment!  *"
+echo "****************************************************"
 
 ROOT_PASSWORD=${ROOT_PASSWORD:-password}
 echo "root:$ROOT_PASSWORD" | chpasswd
@@ -43,6 +19,7 @@ if [ "$#" -eq 0 ]; then
     supervisord -c /etc/supervisord.conf
   fi
 else
+  . /root/.bashrc
   echo " => Executing: $*"
   $*
 fi
