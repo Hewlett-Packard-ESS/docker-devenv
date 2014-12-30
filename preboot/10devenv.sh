728f297deb6a2357dd51688b0a4c43d152cb9295
Karl Stoney <karl.stoney@hp.com>
2014-12-30 16:28:02 +0000

    Fixed an issue with sudo / password setting without tty

--- preboot/10devenv.sh
+++ preboot/10devenv.sh
@@ -5,8 +5,8 @@ echo "****************************************************"
 
 devenv_password=${devenv_password:-password}
 devenv_wemux_password=${devenv_wemux_password:-wemux}
-echo "$devenv_password" | sudo passwd --stdin devenv 2>&1 >/dev/null
-echo "$devenv_wemux_password" | sudo passwd --stdin wemux 2>&1 >/dev/null
+echo "$devenv_password" | passwd --stdin devenv 2>&1 >/dev/null
+echo "$devenv_wemux_password" | passwd --stdin wemux 2>&1 >/dev/null
 
 echo " => Primary logon username: devenv, password: $devenv_password"
 echo " => Wemux logon username: wemux, password: $devenv_wemux_password"
